3d563fe701014a3938695136ba99ffbe24258cae
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2022-09-08 14:31:54 +0000

    update

---
+++ jump.py
@@ -0,0 +1,25 @@
+from stanfordkarel import *
+
+
+def main():
+    """ Karel code goes here! """
+move()
+move()
+move
+turn left()
+move()
+move()
+move()
+move()
+turn left()
+turn left()
+turn left()
+move()
+turn left(
+turn left(
+turn left()
+pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
