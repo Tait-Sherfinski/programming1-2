7546dfea36c71275ab6f3306ee9faf07576d65cc
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2022-09-12 15:27:39 +0000

    update

---
+++ k27.py
@@ -0,0 +1,24 @@
+from stanfordkarel import *
+
+
+def main():
+    """ Karel code goes here! """
+    turn_left()
+    turn_left()
+    move()
+    move()
+    pick_beeper()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    pick_beeper()
+    move()
+    turn_left()
+    
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
