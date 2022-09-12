c8b2775c4d76741f2df55810576d80975bc811b2
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2022-09-12 14:36:12 +0000

    update

--- jump.py
+++ jump.py
@@ -3,22 +3,29 @@ from stanfordkarel import *
 
 def main():
     """ Karel code goes here! """
-move()
-move()
-move()
-turn_left()
-move()
-move()
-move()
-move()
-turn_left()
-turn_left()
-turn_left()
-move()
-turn_left()
-turn_left()
-turn_left()
-pass
+    move()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    pass
 
 
 if __name__ == "__main__":
