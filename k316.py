3445f77857a6b7c70d761406c899eec0c6c91509
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2022-09-19 14:30:24 +0000

    update

---
+++ k316.py
@@ -0,0 +1,38 @@
+from stanfordkarel import *
+
+
+def main():
+    """ Karel code goes here! """
+    pick_beeper()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    turn_left()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    turn_left()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    turn_left()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    move()
+    turn_left()
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
