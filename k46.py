f2c31108e3047e764564189d6599f83e845a7322
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2022-09-20 14:15:54 +0000

    update

---
+++ k46.py
@@ -0,0 +1,106 @@
+from stanfordkarel import *
+
+
+class ktools:
+  def m(self):
+    """Shorthand for move"""
+    move()
+
+    def tl(self):
+      """Shorthand for turn_left"""
+      turn_left()
+
+    def tr(self):
+      """Turn right"""
+      self.tl()
+      self.tl()
+      self.tl()
+
+    def ta(self):
+      """Turn Around"""
+      self.tl()
+      self.tl()
+
+    def pick(self):
+      """Pick Beeper"""
+      pick_beeper()
+
+    def put(self):
+      """Put Beeper"""
+      put_beeper()
+
+    def put2(self):
+      """Put 2 beepers in a line"""
+      self.put()
+      self.m()
+      self.put()
+
+    def put5(self):
+      """Put 5 beepers in a line"""
+      self.put2()
+      self.m()
+      self.put2()
+      self.m()
+      self.put2()
+
+    def hurdlerace(self):
+      """Run the hurdle race for k46"""
+      self.tl()
+      self.m()
+      self.m()
+      self.tr()
+      self.m()
+      self.tr()
+      self.m()
+      self.m()
+      self.ta()
+      self.m()
+      self.tr()
+      self.m()
+      self.tr()
+      self.m()
+      self.tl()
+      self.m()
+      self.tl()
+      self.m()
+      self.m()
+      self.m()
+      self.tr()
+      self.m()
+      self.tr()
+      self.m()
+      self.m()
+      self.m()
+      self.tl()
+      self.m()
+      self.tl()
+      self.m()
+      self.m()
+      self.tr()
+      self.m()
+      self.tr()
+      self.m()
+      self.m()
+      self.tl()
+      self.m()
+      self.tl()
+      self.m()
+      self.m()
+      self.tr()
+      self.m()
+      self.tr()
+      self.m()
+      self.m()
+      self.tl()
+      
+
+
+def main():
+    """ Karel code goes here! """
+    kt = ktools()
+    kt.hurdlerace()
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
