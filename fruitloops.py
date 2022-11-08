ed189490420e4b5412d44e3e49641a81692b1228
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2022-11-08 15:15:22 +0000

    update

---
+++ fruitloops.py
@@ -0,0 +1,13 @@
+colors = ["red", "green", "blue"]
+print(colors)
+print(colors[2])
+
+fruits = ["apple", "banana", "orange", "kiwi", "mango"]
+for fruit in fruits:
+  print(fruit)
+
+for index in range(len(fruits)):
+  print(fruits[index])
+
+lastfruit = fruits[len(fruits) - 1]
+print(lastfruit)
